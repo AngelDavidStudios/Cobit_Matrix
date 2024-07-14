b624a20ac7625b44846ed2ccac247d3836c8e94e
Angel David <angeldavidstudios@outlook.com>
2024-07-14 14:28:52 -0500

    Integridad de la matriz mejorada

--- Cobit_Matrix/Controllers/MatrizController.cs
+++ Cobit_Matrix/Controllers/MatrizController.cs
@@ -9,22 +9,26 @@ public class MatrizController: Controller
 {
     private readonly ApiService<MetaAlineamiento> _metaAlineamientoService;
     private readonly ApiService<MetaEmpresarial> _metaEmpresarialService;
+    private readonly ApiService<ObjetivosGobierno> _ObjetivosGobiernoService;
     
     public MatrizController()
     {
         _metaAlineamientoService = new ApiService<MetaAlineamiento>("http://localhost:5249/api");
         _metaEmpresarialService = new ApiService<MetaEmpresarial>("http://localhost:5249/api");
+        _ObjetivosGobiernoService = new ApiService<ObjetivosGobierno>("http://localhost:5249/api");
     }
     
     public async Task<IActionResult> Index()
     {
         var metaAlineamientos = await _metaAlineamientoService.GetAllAsync("GoalAlignment");
         var metaEmpresariales = await _metaEmpresarialService.GetAllAsync("GoalBusiness");
+        var objetivosGobierno = await _ObjetivosGobiernoService.GetAllAsync("ObjeGovernment");
 
         var viewModel = new MatrizViewModel()
         {
             MetaAlineamientos = metaAlineamientos,
-            MetaEmpresariales = metaEmpresariales
+            MetaEmpresariales = metaEmpresariales,
+            ObjetivosGobierno = objetivosGobierno
         };
 
         return View(viewModel);
--- Cobit_Matrix/ViewModel/MatrizViewModel.cs
+++ Cobit_Matrix/ViewModel/MatrizViewModel.cs
@@ -6,4 +6,5 @@ public class MatrizViewModel
 {
     public IEnumerable<MetaAlineamiento> MetaAlineamientos { get; set; }
     public IEnumerable<MetaEmpresarial> MetaEmpresariales { get; set; }
+    public IEnumerable<ObjetivosGobierno> ObjetivosGobierno { get; set; }
 }
